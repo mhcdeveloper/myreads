56c2c74e73c88a8f782fcac151a55aae7d317ee5
Marcos Henrique <mhcdeveloper@gmail.com>
2017-12-04 11:09:25 -0200

    alteracao nos componentes List e DialogSuccess para componentesfuncional Stateless

--- src/shared/DialogSuccess.js
+++ src/shared/DialogSuccess.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
-class DialogSuccess extends Component {
-    render() {
-        const { open } = this.props;
-        const actions = [
-            <Link to='/'>
-                <FlatButton 
-                    label="Ok"
-                    primary
-                />
-            </Link>
-        ]
-        return (
-            <div>
-                <Dialog
-                    actions={actions}
-                    title="Cadastro efetuado com sucesso!"
-                    modal={true}
-                    open={open}
-                >
-                    O livro foi cadastrado com sucesso!! Clique no botão e retornará para lista dos livros!!
-                </Dialog>
-            </div>
-        )
-    }
-}
+const actions = [
+    <Link to='/'>
+        <FlatButton 
+            label="Ok"
+            primary
+        />
+    </Link>
+];
+const DialogSuccess = ({ open }) => (
+    <div>
+       <Dialog
+            actions={actions}
+            title="Cadastro efetuado com sucesso!"
+            modal={true}
+            open={open}
+        >
+        O livro foi cadastrado com sucesso!! Clique no botão e retornará para lista dos livros!!
+        </Dialog>
+    </div>
+)
 
 export default DialogSuccess;
